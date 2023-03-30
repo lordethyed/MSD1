ae9099d353d5c692c20a5f0ad9620d96e4d1fc88
Temirlan-qa <temirlanaazbek@gmail.com>
2023-03-30 20:36:18 +0600

    BottomNavigationBar

--- lib/presentation/screens/magazine_screen.dart
+++ lib/presentation/screens/magazine_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class MagazineScreen extends StatelessWidget {
+  const MagazineScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/presentation/screens/user_screen.dart
+++ lib/presentation/screens/user_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class UserScreen extends StatelessWidget {
+  const UserScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
