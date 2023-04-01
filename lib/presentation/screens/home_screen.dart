792928de5c566b02278049110ff89eb95931f3d8
Temirlan-qa <temirlanaazbek@gmail.com>
2023-04-01 12:30:51 +0600

    HomeScreen without business logic

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -5,6 +5,31 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _buildCells(int count) {
+      return List.generate(
+        count,
+        (index) => Container(
+          alignment: Alignment.center,
+          width: 120.0,
+          height: 60.0,
+          color: Colors.white,
+          margin: EdgeInsets.all(4.0),
+          child: Text(
+            "${index + 1}",
+          ),
+        ),
+      );
+    }
+
+    List<Widget> _buildRows(int count) {
+      return List.generate(
+        count,
+        (index) => Row(
+          children: _buildCells(10),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -20,7 +45,26 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: _buildCells(20),
+            ),
+            Flexible(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: _buildRows(20),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
